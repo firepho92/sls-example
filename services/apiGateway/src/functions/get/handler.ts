0563d8eeeabecb8baefbf0bd56f02413c3a9097b
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-12-26 19:34:04 -0600

    deleted useless code

--- services/apiGateway/src/functions/get/handler.ts
+++ services/apiGateway/src/functions/get/handler.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import middy from '@middy/core'
 import formatJSONResponse from '../../../../../src/utils/response/formatJSONResponse';
 import httpResponseHandlerMiddleware from '../../../../../src/middleware/httpResponseHandlerMiddleware';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
 
 export const main = middy(async (event: any) => {
   return formatJSONResponse({
