29714c04f415b2a65c440597d2783f00c2f17a81
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-12-26 19:33:45 -0600

    deleted useless code

--- services/apiGateway/src/functions/get/handler.ts
+++ services/apiGateway/src/functions/get/handler.ts
@@ -1,34 +1,10 @@
 import 'reflect-metadata';
-import https from 'http';
-// const https = require('http');
 import middy from '@middy/core'
 import formatJSONResponse from '../../../../../src/utils/response/formatJSONResponse';
 import httpResponseHandlerMiddleware from '../../../../../src/middleware/httpResponseHandlerMiddleware';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
 
-export const main = middy(async (event: any, context: any) => {
-  // console.log('env', process.env);
-  // console.log('context', context);
-  // console.log('event', event.requestContext);
-
-  // const options = {
-  //     hostname: 'localhost',
-  //     port: 8080,
-  //     path: '/cache/SecretDBName',
-  //     method: 'GET',
-  //     headers: {
-  //       'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN
-  //   },
-  // }
-  // const req = https.request(options, res => {
-  //     res.on('data', d => {
-  //         console.log('Response from cache: '+d);
-  //         return d;
-  //     })
-  // });
-  // req.on('error', error => {
-  //     console.error(error)
-  // });
-  // req.end();
+export const main = middy(async (event: any) => {
   return formatJSONResponse({
     event,
   }, 404);
