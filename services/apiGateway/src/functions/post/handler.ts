6766f424f0375937db6e671155df8cdaacf5d63b
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-12-26 20:24:11 -0600

    updated

--- services/apiGateway/src/functions/post/handler.ts
+++ services/apiGateway/src/functions/post/handler.ts
@@ -1,14 +1,14 @@
 import 'reflect-metadata';
 import schema from './schema';
 import middy from '@middy/core';
+import container from './inversify.config';
+import TYPES from '../../../../../src/TYPES';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import Adapter from '../../../../../src/modules/common/adapter/Adapter';
 import formatJSONResponse from '../../../../../src/utils/response/formatJSONResponse';
 import httpResponseHandlerMiddleware from '../../../../../src/middleware/httpResponseHandlerMiddleware';
 import ApiGatewayAdapterParams from '../../../../../src/modules/apiGateway/adapter/ApiGatewayAdapterParams';
 import httpJoiValidatorMiddleware, { VALIDATOR_TYPE } from '../../../../../src/middleware/httpJoiValidatorMiddleware';
-import container from './inversify.config';
-import TYPES from '../../../../../src/TYPES';
 
 export const main = middy(async (event: any) => {
   const adapter: Adapter<ApiGatewayAdapterParams, Promise<ApiGatewayAdapterParams>> = container.get<Adapter<ApiGatewayAdapterParams, Promise<ApiGatewayAdapterParams>>>(TYPES.ApiGatewayAdapter);
