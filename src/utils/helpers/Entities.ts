ae039cefee9821c07c39e01d711bd9734ca3bbbf
Alex Aguilar <aleaguilar@palaceresorts.com>
2022-12-27 19:58:02 -0600

    added entities

--- src/utils/helpers/Entities.ts
+++ src/utils/helpers/Entities.ts
@@ -2,6 +2,5 @@ import Person from '../../../src/modules/apiGateway/domain/entity/Person';
 import Couple from '../../../src/modules/apiGateway/domain/entity/Couple';
 
 export default [
-  Person,
-  Couple
+  Person
 ]
