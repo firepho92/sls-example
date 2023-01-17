1796f5c04bf6d9647bd39d081f3a86257c22e004
Alex Aguilar <aleaguilar@palaceresorts.com>
2023-01-17 16:42:03 -0600

    added default authorizer

--- serverless-compose.ts
+++ serverless-compose.ts
@@ -6,9 +6,13 @@ const serverlessCompose = {
     'architecture-example-layer-db': {
       path: 'layers/db'
     },
+    'default-authorizer': {
+      dependsOn: ['architecture-example-layer-lib', 'architecture-example-layer-db'],
+      path: 'authorizer/lambda-authorizer',
+    },
     'apiGateway': {
       path: 'services/apiGateway',
-      dependsOn: ['architecture-example-layer-lib', 'architecture-example-layer-db'],
+      dependsOn: ['default-authorizer'],
       params: {
         'commonLib': '${architecture-example-layer-lib.CommonArchitectureExampleLibLambdaLayerQualifiedArn}',
         'commonDb': '${architecture-example-layer-db.CommonArchitectureExampleDbLambdaLayerQualifiedArn}',
