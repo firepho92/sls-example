dc9c1b51da4f580464ceccc4327c7ac300d993d8
Alex Aguilar <aleaguilar@palaceresorts.com>
2023-06-06 18:52:14 -0600

    fixed reference

--- services/sqs/src/functions/validCoupleConsumer/index.ts
+++ services/sqs/src/functions/validCoupleConsumer/index.ts
@@ -1,5 +1,5 @@
 import type { AWS } from '@serverless/typescript';
-import { handlerPath } from 'src/lib/handler-resolver';
+import { handlerPath } from '../../lib/handler-resolver';
 
 const lambda: AWS['functions']['k'] = {
   name: '${self:custom.func_prefix}-valid-couple-consumer',
