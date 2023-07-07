96504244bff843ffef34ca8dcdccc51d536c5e8d
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-07 17:51:06 -0600

    added middleware

--- services/sqs/src/functions/invalidCoupleConsumer/handler.ts
+++ services/sqs/src/functions/invalidCoupleConsumer/handler.ts
@@ -1,5 +1,10 @@
 import middy from '@middy/core';
+import eventNormalizerMiddleware from '@middy/event-normalizer';
+// import sqsPartialBatchFailure from '@middy/sqs-partial-batch-failure';
 
 export const main = middy(async (event: any) => {
   console.log('invalid', event);
 });
+
+main
+  .use(eventNormalizerMiddleware());
