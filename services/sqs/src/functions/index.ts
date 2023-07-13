e4a6e826145c2b592a901072ddbcda00b99b36b9
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-13 17:47:37 -0600

    enabled

--- services/sqs/src/functions/index.ts
+++ services/sqs/src/functions/index.ts
@@ -1,9 +1,11 @@
 import coupleDLQConsumer from './coupleDLQConsumer';
-// import validCoupleConsumer from './validCoupleConsumer';
+import validCoupleConsumer from './validCoupleConsumer';
 import invalidCoupleConsumer from './invalidCoupleConsumer';
 
 export default {
-  // validCoupleConsumer,
+  validCoupleConsumer,
   invalidCoupleConsumer,
   coupleDLQConsumer
 }
+
+// 93cf440b-b3e4-49e0-9cc6-2733ec5385b9	arn:aws:lambda:us-east-1:427784172992:function:dev-sqs-invalid-couple-consumer
