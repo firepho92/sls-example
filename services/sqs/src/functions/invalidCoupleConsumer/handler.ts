ef4d596fbf5772471b1ecafb4fc8f9da99a0bc4d
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-13 15:19:47 -0600

    added versions and version middleware

--- services/sqs/src/functions/invalidCoupleConsumer/handler.ts
+++ services/sqs/src/functions/invalidCoupleConsumer/handler.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import TYPES from './TYPES';
 import middy from '@middy/core';
+import versions from './versions';
+import container from './inversify.config';
 import eventNormalizerMiddleware from '@middy/event-normalizer';
 // import sqsPartialBatchFailure from '@middy/sqs-partial-batch-failure';
 import Handler from '../../../../../src/modules/infrastructure/app/Handler';
-import container from './inversify.config';
+import httpRequestVersionHandlerMiddleware from '../../../../../src/middleware/httpRequestVersionHandlerMiddleware';
 
 export const main = middy(async (event: any): Promise<void> => {
   console.log('invalid', event);
@@ -13,4 +15,5 @@ export const main = middy(async (event: any): Promise<void> => {
 });
 
 main
+  .use(httpRequestVersionHandlerMiddleware(versions))
   .use(eventNormalizerMiddleware());
