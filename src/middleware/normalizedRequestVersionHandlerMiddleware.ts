bf517bf3d4705e792192aaa9e108cb062c0553da
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-13 15:49:09 -0600

    updated

--- src/middleware/normalizedRequestVersionHandlerMiddleware.ts
+++ src/middleware/normalizedRequestVersionHandlerMiddleware.ts
@@ -1,6 +1,6 @@
-import Exception from "src/utils/error/Exception";
-import ErrorCode from "src/utils/error/errorCode";
-import HttpStatusCode from "src/utils/enums/httpStatusCode";
+import Exception from 'src/utils/error/Exception';
+import ErrorCode from 'src/utils/error/errorCode';
+import HttpStatusCode from 'src/utils/enums/httpStatusCode';
 
 const normalizedRequestVersionHandlerMiddleware = (versions: Array<string>) => {
   const middlewareBefore = (request: any) => {
