1ad95a5c8b972f0d76493398798f516ae9b937a2
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-17 15:14:54 -0600

    deleted lambda

--- services/sqs/src/functions/index.ts
+++ services/sqs/src/functions/index.ts
@@ -1,11 +1,11 @@
 import coupleDLQConsumer from './coupleDLQConsumer';
 // import validCoupleConsumer from './validCoupleConsumer';
-import invalidCoupleConsumer from './invalidCoupleConsumer';
+// import invalidCoupleConsumer from './invalidCoupleConsumer';
 
 export default {
   coupleDLQConsumer,
   // validCoupleConsumer,
-  invalidCoupleConsumer
+  // invalidCoupleConsumer
 }
 
 // 93cf440b-b3e4-49e0-9cc6-2733ec5385b9	arn:aws:lambda:us-east-1:427784172992:function:dev-sqs-invalid-couple-consumer
