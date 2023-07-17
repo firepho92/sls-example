75900cebb0af0b88056f04c4623c257aa12dda1b
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-17 16:19:46 -0600

    decreased batch size 1

--- services/sqs/src/functions/invalidCoupleConsumer/index.ts
+++ services/sqs/src/functions/invalidCoupleConsumer/index.ts
@@ -9,7 +9,7 @@ const lambda: AWS['functions']['k'] = {
     {
       sqs: {
         arn: '${env:SqsDemoSingleExampleSQSArn}',
-        batchSize: 10,
+        batchSize: 1,
         filterPatterns: [
           {
             body: {
