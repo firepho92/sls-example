88ff1079087c39aa56f172b3c53c0b144aa564fa
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 15:21:08 -0600

    updated returned value to match error format

--- src/modules/sqs/app/InvalidCoupleHandler.ts
+++ src/modules/sqs/app/InvalidCoupleHandler.ts
@@ -19,8 +19,8 @@ export default class InvalidCoupleHandler extends NormalizedEventBaseHandler<any
     console.log('InvalidCoupleHandler', JSON.stringify(port));
     const responses = port.Records.map(async (record) => {
       console.log('record', record);
-      const response = await this.adapter.execute(record.body);
-      return response;
+      await this.adapter.execute(record.body);
+      return record;
     });
     const retryItems = (await Promise.allSettled(responses)).filter((item) => item.status === PromiseStatus.REJECTED);
     console.log('Promise response', JSON.stringify(retryItems));
