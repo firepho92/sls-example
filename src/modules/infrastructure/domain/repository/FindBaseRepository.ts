5be7476cc447e371674e6d5fa66467f584d21338
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 12:31:45 -0600

    deleted useless code

--- src/modules/infrastructure/domain/repository/FindBaseRepository.ts
+++ src/modules/infrastructure/domain/repository/FindBaseRepository.ts
@@ -1,15 +1,11 @@
 import 'reflect-metadata';
-import { injectable, unmanaged } from 'inversify';
-import { DataSource, SelectQueryBuilder } from 'typeorm';
-import DBConnectionManager from '../../../../shared/database/DBConnectionManager';
-//import Log from '../../../../../shared/utils/logger/implementation/Log';
-import Warning from '../../../../shared/error/Warning';
-import ErrorCode from '../../../../shared/error/ErrorCode';
-import Exception from '../../../../shared/error/Exception';
-import HttpStatusCode from '../../../../shared/enums/HttpStatusCode';
-import PostgresSQLErrorCodes from '../../../../shared/enums/PostgresSQLErrorCodes';
-
-export type ObjectType<T> = { new(): T } | Function; // eslint-disable-line
+import { injectable } from 'inversify';
+import { SelectQueryBuilder } from 'typeorm';
+import PostgresSQLErrorCodes from 'src/utils/enums/postgresSQLErrorCodes';
+import HttpStatusCode from 'src/utils/enums/httpStatusCode';
+import ErrorCode from 'src/utils/error/errorCode';
+import Warning from 'src/utils/error/Warning';
+import Exception from 'src/utils/error/Exception';
 
 /**
  * @absract class FindBaseRepository
@@ -19,19 +15,12 @@ export type ObjectType<T> = { new(): T } | Function; // eslint-disable-line
  * @implements FindBaseRepository<T>
  * @description Base repository class
  * @author Raul Dominguez
- * @created 2022-07-18
- * @updated 2022-07-18
- * @updatedBy Raul Dominguez
+ * @created 2023-07-18
+ * @updated 2023-07-18
+ * @updatedBy Alex Aguilar
  */
 @injectable()
 export default abstract class FindBaseRepository<T> {
-  // private type: ObjectType<T>;
-  // private iDBConnectionManager: DBConnectionManager;
-
-  // constructor(@unmanaged() type: ObjectType<T>, @unmanaged() iDBConnectionManager: DBConnectionManager) {
-  //   this.iDBConnectionManager = iDBConnectionManager;
-  //   this.type = type;
-  // }
   protected abstract buildQuery(port?: T): Promise<SelectQueryBuilder<T>>;
 
   /**
@@ -48,14 +37,6 @@ export default abstract class FindBaseRepository<T> {
       const query = await this.buildQuery() as SelectQueryBuilder<T>;
       const entities: T[] = await query.getMany();
       return entities;
-      // console.log('🚀 ~ file: FindBaseRepository.ts ~ line 46 ~ execute ~ start');
-      // const connection: DataSource = await this.iDBConnectionManager.connect();
-      // //Build up the query
-      // const query: SelectQueryBuilder<T> = connection.manager.createQueryBuilder(this.type, 'entity');
-      // //Execute the query
-      // const entities: T[] = await query.getMany();
-      // console.log('🚀 ~ file: FindBaseRepository.ts ~ line 52 ~ execute ~ end');
-      // return entities;
     } catch (error) {
       console.log('🚀 ~ file: FindBaseRepository.ts ~ line 55 ~ execute ~ error', error);
       if (error.code === PostgresSQLErrorCodes.FOREIGN_KEY_VIOLATION)
