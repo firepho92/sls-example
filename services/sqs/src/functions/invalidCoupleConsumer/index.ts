281ab6f75d8f978f363e7792ed29cc2f6216a870
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 11:32:09 -0600

    updated timeout to match sqs visibility time

--- services/sqs/src/functions/invalidCoupleConsumer/index.ts
+++ services/sqs/src/functions/invalidCoupleConsumer/index.ts
@@ -5,6 +5,7 @@ const lambda: AWS['functions']['k'] = {
   name: '${self:custom.func_prefix}-invalid-couple-consumer',
   handler: `${handlerPath(__dirname)}/handler.main`,
   logRetentionInDays: 14,
+  timeout: 30,
   events: [
     {
       sqs: {
