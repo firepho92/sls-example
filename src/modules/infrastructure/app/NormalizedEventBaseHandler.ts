67ad2efb7e531946fb6a3bc8c9997c7db99ef23a
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 21:19:37 -0600

    updated

--- src/modules/infrastructure/app/NormalizedEventBaseHandler.ts
+++ src/modules/infrastructure/app/NormalizedEventBaseHandler.ts
@@ -6,12 +6,12 @@ import { PromiseStatus } from 'src/utils/enums/PromiseStatus';
 
 //TODO: Renombrar a SQSEventBaseHandler
 @injectable()
-export default abstract class NormalizedEventBaseHandler<T, U, V> implements Handler<SQSEvent, V> {
+export default abstract class NormalizedEventBaseHandler<T={}, U={}, V={}> implements Handler<SQSEvent, SQSRecord[] | V> {
   constructor(){}
 
   protected abstract run(port?: SQSEvent): Promise<PromiseSettledResult<SQSRecord>[]>;
 
-  async execute(port?: SQSEvent): Promise<V> {
+  async execute(port?: SQSEvent): Promise<SQSRecord[] | V> {
     const items = await this.run(port);
     console.log('items', items);
     const retryItems = port.Records.filter((record) => {
@@ -26,6 +26,6 @@ export default abstract class NormalizedEventBaseHandler<T, U, V> implements Han
     // return retryItems;
     // port.Records.forEach((record) => {console.log('record.body', record.body)});
     console.log('NormalizedEventBaseHandler', retryItems);
-    return null;
+    return retryItems;
   }
 }
