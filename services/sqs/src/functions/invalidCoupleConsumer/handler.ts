fd330ae5bf20c2b6319fabd5b7801e64a8e76abf
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 21:33:46 -0600

    returning failed messages

--- services/sqs/src/functions/invalidCoupleConsumer/handler.ts
+++ services/sqs/src/functions/invalidCoupleConsumer/handler.ts
@@ -3,16 +3,18 @@ import TYPES from './TYPES';
 import middy from '@middy/core';
 import versions from './versions';
 import container from './inversify.config';
+import { SQSEvent, SQSRecord } from 'aws-lambda';
 import eventNormalizerMiddleware from '@middy/event-normalizer';
 // import sqsPartialBatchFailure from '@middy/sqs-partial-batch-failure';
 import Handler from '../../../../../src/modules/infrastructure/app/Handler';
 import normalizedRequestVersionHandlerMiddleware from '../../../../../src/middleware/normalizedRequestVersionHandlerMiddleware';
 
-export const main = middy(async (event: any): Promise<void> => {
+export const main = middy(async (event: any): Promise<SQSRecord[]> => {
   // console.log('invalid', event);
-  const handler =  container.get<Handler>(TYPES[event.version]);
+  const handler =  container.get<Handler<SQSEvent, SQSRecord[]>>(TYPES[event.version]);
   const response = await handler.execute(event);
   console.log('handler', response);
+  return response;
 });
 
 main
