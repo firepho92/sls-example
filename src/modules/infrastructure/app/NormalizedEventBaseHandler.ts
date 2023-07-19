7a1bc0c908e272bcd2ca6e3a360746d276bf5043
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-18 20:17:24 -0600

    updated

--- src/modules/infrastructure/app/NormalizedEventBaseHandler.ts
+++ src/modules/infrastructure/app/NormalizedEventBaseHandler.ts
@@ -11,8 +11,18 @@ export default abstract class NormalizedEventBaseHandler<T> implements Handler<S
 
   async execute(port?: SQSEvent): Promise<SQSBatchResponse> {
     const response = await this.run(port);
-    port.Records.forEach((record) => {console.log('record.body', record.body)});
-    console.log('NormalizedEventBaseHandler')
+    // const retryItems = response.filter((record) => {
+    //   return !items.find((item) => {
+    //     return item.status === PromiseStatus.FULFILLED && item.value === record;
+    //   });
+    // });
+    // const retryItems = port.Records.filter((record) => {
+      
+    // });
+    // console.log('Promise response', JSON.stringify(retryItems));
+    // return retryItems;
+    // port.Records.forEach((record) => {console.log('record.body', record.body)});
+    console.log('NormalizedEventBaseHandler');
     return null;
   }
 }
