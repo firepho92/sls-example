c128e6266de421e96120f7bf0b91cfac256839a8
aleaguilar <aleaguilar@palaceresorts.com>
2023-07-27 16:25:45 -0600

    deleted useless dto

--- src/modules/infrastructure/controller/APIGatewayProxyEventHandlerParams.ts
+++
@@ -1,6 +0,0 @@
-import { ValidationInput } from 'src/middleware/httpJoiValidatorMiddleware';
-import { APIGatewayProxyEvent } from 'aws-lambda/trigger/api-gateway-proxy';
-
-export default interface APIGatewayProxyEventHandlerParams {
-  event: APIGatewayProxyEvent
-}
