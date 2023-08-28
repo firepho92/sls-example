f829c82a1697039c65aa7628131fd9bbc8df182d
aleaguilar <aleaguilar@palaceresorts.com>
2023-08-28 10:50:44 -0600

    added connection management

--- src/modules/apiGateway/useCase/ApiGatewayGetUseCase.ts
+++ src/modules/apiGateway/useCase/ApiGatewayGetUseCase.ts
@@ -6,21 +6,26 @@ import UseCase from '../../../../src/modules/infrastructure/useCase/UseCase';
 import Repository from '../../../../src/modules/infrastructure/domain/repository/Repository';
 import PaginationQueryDTO from '../../../../src/modules/infrastructure/domain/dto/PaginationQueryDTO';
 import FindManyPaginatedBaseRepositoryParams from '../../../../src/modules/infrastructure/domain/repository/FindManyPaginatedBaseRepositoryParams';
+import DBConnectionManager from 'src/utils/database/DBConnectionManager';
 
 @injectable()
 export default class ApiGatewayGetUseCase implements UseCase<PaginationQueryDTO, Promise<FindManyPaginatedBaseRepositoryParams<Couple>>> {
   constructor(
+    @inject(TYPES.DBConnectionManager) private dbConnectionManager: DBConnectionManager,
     @inject(TYPES.CoupleFindPaginatedRepository) private coupleFindPaginatedRepository: Repository<PaginationQueryDTO, Promise<FindManyPaginatedBaseRepositoryParams<Couple>>>
   ) { }
 
   async execute(port?: PaginationQueryDTO): Promise<FindManyPaginatedBaseRepositoryParams<Couple>> {
     console.log('ApiGatewayGetUseCase execute', port);
+    await this.dbConnectionManager.connect();
     try {
-      const items = await this.coupleFindPaginatedRepository.execute(port);
+      const items: FindManyPaginatedBaseRepositoryParams<Couple> = await this.coupleFindPaginatedRepository.execute(port);
       return items;
     } catch (error) {
       console.error(error);
       throw error;
+    } finally {
+      await this.dbConnectionManager.disconnect();
     }
   }
 }
