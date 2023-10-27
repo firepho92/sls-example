a26d5c71c4aceeac6d7dd819ef99ea09df9fdeb2
Ivan Balam <ivbalam@palaceresorts.com>
2023-10-27 12:31:10 -0600

    fix: remove unused provider properties on subscription template

--- subscription/serverless.ts
+++ subscription/serverless.ts
@@ -5,18 +5,6 @@ const serverlessConfiguration: AWS = {
   frameworkVersion: "3",
   provider: {
     name: "aws",
-    runtime: "nodejs16.x",
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
-      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000"
-    },
-    deploymentBucket: {
-      name: '${ssm:s3_bucket_deploy_sls, "s3_bucket_deploy_sls"}'
-    },
     iam: {
       role: '${env:iamRole, "iamRole"}'
     }
